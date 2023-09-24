03d6650e59b1f9a1bbe909241fafad126492e754
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-09-25 03:03:24 +0600

    first commit

---
+++ src/Root.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header/Header";
+
+
+const Root = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Root;
